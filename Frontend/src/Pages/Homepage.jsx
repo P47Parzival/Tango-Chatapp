54e9868608f20804301926e4d341a2db79fc0f14
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-01 22:33:32 +0530

    Update Homepage.jsx

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
         <div className='flex flex-col justify-center items-center h-screen'>
             <div className='text-center'>
                 <h1 className='text-black pb-5'>Welcome to Tango-Chat, your one step application to chat across the world</h1>
-                <Link to="/signup">
+                <Link to="/register">
                     <button className='h-20 pl-4 pr-4 border-solid border-2 rounded-3xl border-black bg-blue-500 text-white'>Start Chatting</button>
                 </Link>
             </div>
